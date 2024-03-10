f57a4591bedf12ee5bcfc01ac3ec0f10820201cb
BrahimElhajji <elhajji080898@gmail.com>
2024-03-10 06:40:37 -0700

    fixed basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,29 @@
 #!/usr/bin/python3
 """
-BaseModel
-
-"""
+BaseModel file"""
 from datetime import datetime
-import uuid
-import models
-<<<<<<< HEAD
-import datetime
-import uuid
-=======
->>>>>>> 2250569be11eda7a65e15a6f69c4a2938bdf60e3
+import models 
+from uuid import uuid4
 
 
 class BaseModel:
-    """
-    a class BaseModel that defines
-    all common attributes/methods for other classes
-    """
+    """BaseModel Class"""
+    
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instances of the BaseModel
+        """Initialize a new BaseModel.
+
         Args:
-            @id:
-            @created_at:
-            @updated_at:
+            *args
+            **kwargs : Key/value pairs of attributes in dictionary.
         """
-<<<<<<< HEAD
         datetimeform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] =
-                    datetime.datetime.strptime(v, datetimeform)
-=======
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.fromisoformat(v)
->>>>>>> 2250569be11eda7a65e15a6f69c4a2938bdf60e3
+                    self.__dict__[k] = datetime.strptime(v, datetimeform)
                 else:
                     self.__dict__[k] = v
         else:
@@ -58,14 +37,13 @@ class BaseModel:
     def save(self):
         """ updates the public instance attribute updated_at with
         the current time"""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """a dictionary containing all keys/values
         of __dict__ of the instance"""
         dic_inst = self.__dict__.copy()
-        dic_inst["id"] = self.id
         dic_inst["created_at"] = self.created_at.isoformat()
         dic_inst["updated_at"] = self.updated_at.isoformat()
         dic_inst["__class__"] = self.__class__.__name__
